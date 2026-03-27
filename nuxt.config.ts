0424d3a883605d45a52501c9a41252907df31e6f
farzeen-ali <farzeenaliarif786@gmail.com>
2026-03-27 22:16:18 +0500

    Integration Supabase

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabasePublishableKey: process.env.SUPABASE_PUBLISHABLE_KEY,
+    },
+  },
+});
