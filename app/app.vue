15e5c263c0a90de32a6aef57fa9bc6d98ae48376
farzeen-ali <farzeenaliarif786@gmail.com>
2026-04-26 23:02:11 +0500

    Insert Data into Supabas ePostgreSQL DB

--- app/app.vue
+++ app/app.vue
@@ -3,10 +3,24 @@ import { createClient } from '@supabase/supabase-js'
 const config = useRuntimeConfig()
 const supabase = createClient(config.public.supabaseUrl, config.public.supabasePublishableKey)
 const instruments = ref([])
+const form = ref({ name: ''})
 
 async function getInstruments() {
   const { data } = await supabase.from('instruments').select()
   instruments.value = data
+}
+
+async function addInstrument(){
+  if(!form.value?.name) return;
+  const { error } = await supabase
+    .from('instruments')
+    .insert([ { name: form.value.name }])
+    if(!error){
+      form.value.name = ''
+      getInstruments();
+    } else{
+      console.log(error)
+    }
 }
 
 onMounted(() => {
@@ -15,7 +29,13 @@ onMounted(() => {
 </script>
 
 <template>
+  <div>
+    <form @submit.prevent="addInstrument">
+      <input type="text" v-model="form.name" placeholder="Enter instrument name">
+      <button type="submit">Add</button>
+    </form>
   <ul>
     <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
   </ul>
+    </div>
 </template>
