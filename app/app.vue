02f88f5befd64ee5bf4187beeea964f43accd5e4
farzeen-ali <farzeenaliarif786@gmail.com>
2026-04-28 22:08:42 +0500

    Update Data

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,10 @@ const supabase = createClient(config.public.supabaseUrl, config.public.supabaseP
 const instruments = ref([])
 const form = ref({ name: ''})
 
+const isEditMode = ref(false);
+
+const editId = ref(null)
+
 async function getInstruments() {
   const { data } = await supabase.from('instruments').select()
   instruments.value = data
@@ -12,6 +16,21 @@ async function getInstruments() {
 
 async function addInstrument(){
   if(!form.value?.name) return;
+  if(isEditMode.value){
+    const { error } = await supabase
+    .from('instruments')
+    .update({ name: form.value.name })
+    .eq('id', editId.value)
+
+    if(!error){
+      form.value.name = ''
+      isEditMode.value = false;
+      editId.value = null;
+      getInstruments()
+    } else {
+      console.log(error)
+    }
+  } else {
   const { error } = await supabase
     .from('instruments')
     .insert([ { name: form.value.name }])
@@ -21,8 +40,14 @@ async function addInstrument(){
     } else{
       console.log(error)
     }
+  }
 }
 
+function editInstrument(item){
+  form.value.name = item.name;
+  isEditMode.value = true;
+  editId.value = item.id
+}
 onMounted(() => {
   getInstruments()
 })
@@ -32,10 +57,15 @@ onMounted(() => {
   <div>
     <form @submit.prevent="addInstrument">
       <input type="text" v-model="form.name" placeholder="Enter instrument name">
-      <button type="submit">Add</button>
+      <button type="submit">
+        {{ isEditMode ? 'Update' : 'Add' }}
+      </button>
     </form>
   <ul>
-    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}
+      <button @click="editInstrument(instrument)">Edit</button>
+    </li>
+    
   </ul>
     </div>
 </template>
